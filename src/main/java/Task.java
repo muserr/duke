333c0a411a9ef31e13cc96f88384919d41a63490
Benjamin Ng <j.k@u.nus.edu>
2019-08-24 15:35:28 +0800

    Using class to represent Task

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,23 +1,40 @@
+/**
+ * Represents a task from user.
+ */
 public class Task {
     protected String description;
     protected boolean isDone;
 
+    /**
+     * Default constructor.
+     * @param description Description of task provided
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * Obtain task status as an icon.
+     * @return isDone Icon representation of isDone
+     */
     public String getStatusIcon() {
-        // return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
-        return (isDone ? "Y" : "N"); //return Y for complete or X for incomplete
+        // Return Y for complete or N for incomplete
+        return (isDone ? "Y" : "N");
     }
 
+    /**
+     * Modifies status variable.
+     * @return description Description of task
+     */
     public String getStatusDescription() {
         return this.description;
     }
 
+    /**
+     * Assigns a completed status to isDone.
+     */
     public void setComplete() {
         this.isDone = true;
     }
-
 }
