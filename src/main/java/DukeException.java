c54165caec4d1e33e71155d40f72364e72d69caa
Benjamin Ng <j.k@u.nus.edu>
2019-09-09 21:10:57 +0800

    Includes DukeException class V1

---
+++ src/main/java/DukeException.java
@@ -0,0 +1,13 @@
+/**
+ * Custom DukeException class.
+ */
+public class DukeException extends Exception {
+
+    /**
+     * Initializes the required message for exception.
+     * @param message Error message
+     */
+    public DukeException(String message) {
+        super(message);
+    }
+}
